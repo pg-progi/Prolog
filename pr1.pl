e6e6f392657e03fb9daa25e1127172c6a5414947
Vlad3d <604659@mail.ru>
2016-09-22 19:31:41 +0300

    pl f e => let[x->e] in f x

--- pr1.pl
+++ pr1.pl
@@ -11,7 +11,7 @@ removeInd(A) :- retract(curind(A)).
 norm(E1, E2) :- atom(E1),  E2 = E1.
 norm(lam(X, E), lam(X,E2)) :- norm(E, E2).
 norm(E1<*>E2, E3) :- norm(E1, E11), atom(E2),!, E3=(E11<*>E2).
-norm(E1<*>E2, E3) :- norm(E1, E11), norm(let([z->E2],E11),let([],E3)).
+norm(E1<*>E2, E3) :- norm(E1, E11), substitute(E2,z,E2,R), norm(let([R->E2],E11),let([],E3)).
 norm(let([], _), _).
 norm(let([(L->T)|LS], E1), let(X, E2)) :- norm(T, ET), append(X, [L->ET], R), norm(let(LS, E1), let(R, E2)), norm(E1, E2), writeln(R).
 norm(E1<*>E2, E3) :-  cnt(K), KK is K+1, atom_concat('Z', KK, R), corAdd(R, E2), E3=(E1<*>R).
